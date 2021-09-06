dd4b8244acb57638ba280f833b81e7c696827e57
Steve Korshakov <steve@korshakov.com>
2021-09-06 18:45:48 +0300

    fix: fix build

--- src/tests/createTestWallet.ts
+++ src/tests/createTestWallet.ts
@@ -2,14 +2,14 @@ import { TonClient } from "..";
 import { delay } from "../utils/time";
 import { openTestTreasure } from "./openTestTreasure";
 
-export async function createTestWallet(client: TonClient, amount: number) {
+export async function createTestWallet(client: TonClient, value: number) {
     let treasure = await openTestTreasure(client);
     const wallet = await client.createWallet();
     const seqno = await treasure.wallet.getSeqNo();
     await treasure.wallet.transfer({
         to: wallet.wallet.address,
         seqno: seqno,
-        amount: amount,
+        value: value,
         secretKey: treasure.secretKey
     });
     while (true) {
