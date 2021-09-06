c23acf1df53fd8feebbb6c39e8bca2cd7ccf8513
Steve Korshakov <steve@korshakov.com>
2021-09-06 18:49:29 +0300

    fix: export Cell object

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { BitString } from './boc/BitString';
+export { Cell } from './boc/Cell';
 export { TonClient } from './client/TonClient';
 export { TonWallet } from './client/TonWallet';
 export { Address } from './address/Address';
