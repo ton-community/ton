8220bee113f815a38b7b5cdcf48b36f7b0a176b1
Steve Korshakov <steve@korshakov.com>
2021-09-09 17:09:21 +0300

    feat: export toNano/fromNano

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { Cell } from './boc/Cell';
 export { TonClient } from './client/TonClient';
 export { Wallet } from './client/Wallet';
 export { Address } from './address/Address';
+export { toNano, fromNano } from './utils/convert';
