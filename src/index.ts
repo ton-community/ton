377ff0a3b504bb5566b8d608c9ed430ce11e7537
Steve Korshakov <steve@korshakov.com>
2021-09-21 13:23:39 +0300

    feat: export WalletContractType

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { BitString } from './boc/BitString';
 export { Cell } from './boc/Cell';
 export { TonClient } from './client/TonClient';
-export { Wallet, validateWalletType } from './client/Wallet';
+export { Wallet, validateWalletType, WalletContractType } from './client/Wallet';
 export { Address } from './address/Address';
 export { toNano, fromNano } from './utils/convert';
 export { KeyStore, KeyRecord } from './keystore/KeyStore';
