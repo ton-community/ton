f29a7005beee84659100d18fbd4c1d739bbaa8fa
Steve Korshakov <steve@korshakov.com>
2022-06-13 09:09:43 +0400

    feat: customizable safeSign seed

--- src/client/safeSign.ts
+++ src/client/safeSign.ts
@@ -1,14 +1,24 @@
 import { sha256_sync, sign, signVerify } from "ton-crypto";
 import { Cell } from "../boc/Cell";
 
-function createSafeSignHash(cell: Cell) {
-    return sha256_sync(Buffer.concat([Buffer.from([0xff, 0xff]), Buffer.from('ton-safe-sign-magic'), cell.hash()]));
+const MIN_SEED_LENGTH = 8;
+const MAX_SEED_LENGTH = 64;
+
+function createSafeSignHash(cell: Cell, seed: string) {
+    let seedData = Buffer.from(seed);
+    if (seedData.length > MAX_SEED_LENGTH) {
+        throw Error('Seed can\t be longer than 64 bytes');
+    }
+    if (seedData.length < MIN_SEED_LENGTH) {
+        throw Error('Seed must be at least 8 bytes');
+    }
+    return sha256_sync(Buffer.concat([Buffer.from([0xff, 0xff]), seedData, cell.hash()]));
 }
 
-export function safeSign(cell: Cell, secretKey: Buffer) {
-    return sign(createSafeSignHash(cell), secretKey);
+export function safeSign(cell: Cell, secretKey: Buffer, seed: string = 'ton-safe-sign-magic') {
+    return sign(createSafeSignHash(cell, seed), secretKey);
 }
 
-export function safeSignVerify(cell: Cell, signature: Buffer, publicKey: Buffer) {
-    return signVerify(createSafeSignHash(cell), signature, publicKey);
+export function safeSignVerify(cell: Cell, signature: Buffer, publicKey: Buffer, seed: string = 'ton-safe-sign-magic') {
+    return signVerify(createSafeSignHash(cell, seed), signature, publicKey);
 }
